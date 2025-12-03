5ac01f286ceeb7f293584cda53a804db77cc78d7
anubha25 <Anubhav.ty11@gmail.com>
2025-12-03 23:49:02 +0530

    Find Largest Function Added

--- Templates_Container.cpp
+++ Templates_Container.cpp
@@ -3,6 +3,19 @@
 #include<string>
 using namespace std;
 
+
+template<typename Container>
+typename Container::value_type Largest(Container Ctr)
+{
+    typename Container::value_type Large={};
+    for(typename Container::value_type &val : Ctr)
+        if(val>Large)
+            Large=val;
+    return Large;
+}
+
+
+
 template<typename Type1>
 typename Type1::value_type sum(Type1 &ctr)
 {
@@ -14,8 +27,17 @@ typename Type1::value_type sum(Type1 &ctr)
 // (typename Type1:: value_type ) is to find the datatype of the values that are inside the Container
 int main()
 {
+   //to find sum :
    vector<int> num = {2,3,4,5};
    vector<string> str = {"Anu","bhav","Tya","gi"};
    cout<<sum(num)<<endl;
-   cout<<sum(str);
+   cout<<sum(str)<<endl;
+
+
+   //to find the Largest of Value from Any Container
+    vector<int> vec={1,2,3,4,5,6};
+    vector<string>vec1={"Anu","Bhav","Tyagi"};
+    cout<<Largest(vec1)<<endl;
+    cout<<Largest(vec);
+
 }
